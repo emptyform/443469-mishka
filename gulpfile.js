622ff7a0b3d1c262bb44b827aacb152b5f05a08c
Tanya Labetskaya <tanyapotapchik@gmail.com>
2017-06-20 17:22:52 +0300

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,12 @@ var sourcemaps = require('gulp-sourcemaps');
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var mqpacker = require("css-mqpacker");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
 var server = require("browser-sync").create();
+var run = require("run-sequence");
 
 gulp.task("style", function() {
   gulp.src("sass/style.scss")
@@ -17,10 +22,25 @@ gulp.task("style", function() {
     .pipe(postcss([
       autoprefixer({browsers: [
         "last 2 versions"
-      ]})
+      ]}),
+      mqpacker ({
+        sort: true
+      })
     ]))
     .pipe(gulp.dest("css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("css"))
     .pipe(server.stream());
+});
+
+gulp.task("images", function () {
+  return gulp.src("img/**/*.{png,jpg,gif}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true})
+    ]))
+    .pipe(gulp.dest("img"));
 });
 
 gulp.task("serve", ["style"], function() {
@@ -35,3 +55,8 @@ gulp.task("serve", ["style"], function() {
   gulp.watch("sass/**/*.{scss,sass}", ["style"]);
   gulp.watch("*.html").on("change", server.reload);
 });
+
+gulp.task("build", function(fn){
+  run("style", "images", fn);
+});
+
