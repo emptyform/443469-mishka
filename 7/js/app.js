57ee2480419dea7ce0cc423a90de53174ebd07a2
Keks <keks@htmlacademy.ru>
2017-06-21 09:50:52 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/app.js
@@ -0,0 +1,32 @@
+var mainNav = document.querySelector(".main-nav");
+var navToggle = mainNav.querySelector(".main-nav__toggle");
+
+mainNav.classList.remove("main-nav--nojs");
+mainNav.classList.remove("main-nav--open");
+
+navToggle.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  if (mainNav.classList.contains("main-nav--open")) {
+    mainNav.classList.remove("main-nav--open");
+
+  } else {
+    mainNav.classList.add("main-nav--open");
+  }
+});
+
+var promoOrderBtn = document.querySelector(".promo__order, .catalog-item__cart");
+var popup = document.querySelector(".popup");
+
+if(promoOrderBtn) {
+  promoOrderBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    if (popup.classList.contains("hidden")) {
+      popup.classList.remove("hidden");
+
+    } else {
+      popup.classList.add("hidden");
+    }
+  });
+}
